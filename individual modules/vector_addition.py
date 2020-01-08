757165060500a1d9140819cd18f10f1b5fbe268a
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-01-08 22:11:07 +0100

    add vector addition

---
+++ individual modules/vector_addition.py
@@ -0,0 +1,9 @@
+def vector_addition(vector_1, vector_2):
+    for element in range(len(vector_2)):
+        try:
+            vector_1[element] += vector_2[element]
+        except IndexError:
+            vector_1.append(vector_2[element])
+
+    return vector_1
+
