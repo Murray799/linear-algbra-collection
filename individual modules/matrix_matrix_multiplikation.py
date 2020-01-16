ec99b69daf30b501a028a4e846c053fdda980863
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-01-16 22:13:23 +0100

    Add files via upload

---
+++ individual modules/matrix_matrix_multiplikation.py
@@ -0,0 +1,16 @@
+def MM_multiplication(matrix_1, matrix_2):
+    #needs square matrix
+
+    end_matrix = []
+    for element in range(len(matrix_1)):
+        end_matrix.append([])
+        for none in matrix_1:
+            end_matrix[element].append(0)
+
+    for i in range(len(matrix_1)):
+        for j in range(len(matrix_1)):
+            for k in range(len(matrix_1)):
+                end_matrix[i][j] += matrix_1[i][k] * matrix_2[k][j]
+
+    return end_matrix
+
