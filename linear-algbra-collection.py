b4e43b18f0bd84cc252c2acf0c510eb1ba1aa774
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-01-17 23:31:05 +0100

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -34,3 +34,19 @@ def vector_addition(vector_1, vector_2):
             vector_1.append(vector_2[element])
 
     return vector_1
+
+def MM_multiplication(matrix_1, matrix_2):
+    #needs square matrix
+
+    end_matrix = []
+    for element in range(len(matrix_1)):
+        end_matrix.append([])
+        for none in matrix_1:
+            end_matrix[element].append(0)
+
+    for i in range(len(matrix_1)):
+        for j in range(len(matrix_1)):
+            for k in range(len(matrix_1)):
+                end_matrix[i][j] += matrix_1[i][k] * matrix_2[k][j]
+
+    return end_matrix
