4b8366a2ed73a2716197e1a44ff3497b2d71fa3f
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-01-31 23:44:50 +0100

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -50,6 +50,42 @@ def MM_multiplication(matrix_1, matrix_2):
 
     return end_matrix
 
+def rowEchelon(M):
+    def normalise(M):
+        new_M = []
+        for element in range(len(M)):
+            new_M.append([])
+            for element2 in range(len(M)):
+                new_M[element].append(M[element2][element])
+        return new_M
+
+    def rowMod(M, i, j, x):
+        M[i] = [a + x * b for a, b in zip(M[i], M[j])]
+
+    M = normalise(M)
+    row, col = 0, 0
+    rows, cols = len(M), len(M[0])
+    while row < rows and col < cols:
+        if M[row][col] == 0:
+            for r in range(row +1, rows):
+                if M[r][col] != 0:
+                    rowMod(M, row, r, 1)
+                    break
+
+        if M[row][col] == 0:
+            col += 1
+            continue
+        pivot = M[row][col]
+
+        for r in range(row +1, rows):
+            if M[r][col] != 0:
+                rowMod(M, r, row, -M[r][col] / pivot)
+
+        row += 1
+        col += 1
+
+    return normalise(M)
+
 def determinant(M):
     def normalise(M):
         new_M = []
