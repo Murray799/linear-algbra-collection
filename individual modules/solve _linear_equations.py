b0c9e81df2478dd47af164688cc7873721a0f7bf
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-02-23 00:35:38 +0100

    solve _linear_equations
    not sure if it works with all matrixis

---
+++ individual modules/solve _linear_equations.py
@@ -0,0 +1,32 @@
+def solve_eq(matrix, result):
+    def abs(lis):
+        if lis < 0:
+            lis = lis*-1
+        return lis
+
+
+    for k in range(len(result)):
+        if abs(matrix[k][k]) < 0:
+            for i in range(k+1, len(result)):
+                if abs(matrix[i][k]) > abs(matrix[k][k]):
+                    for j in range(k,len(result)):
+                        matrix[k][j], matrix[i][j] = matrix[i][j], matrix[k][j]
+                    result[k], result[i] = result[i], result[k]
+                    break
+        pivot = matrix[k][k]
+        for j in range(k, len(result)):
+            matrix[k][j] /= pivot
+        result[k] /= pivot
+
+        for i in range(len(result)):
+            if i == k or matrix[i][k] == 0:
+                continue
+            factore = matrix[i][k]
+            for j in range(k, len(result)):
+                matrix[i][j] -= factore * matrix[k][j]
+            result[i] -= factore*result[k]
+    return matrix, result
+
+print(solve_eq([[2,2],[2,1]],[6,5]))
+
+
