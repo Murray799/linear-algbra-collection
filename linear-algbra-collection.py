4ae9eba8196e8b2c7076831e12f66f37caabd993
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-02-23 00:41:50 +0100

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -102,21 +102,6 @@ def determinant(M):
     return num
 
 def half_rotation(matrix):
-
-    def MM_multiplication(matrix_1, matrix_2):
-        end_matrix = []
-        for element in range(len(matrix_1)):
-            end_matrix.append([])
-            for no_use in matrix_1:
-                end_matrix[element].append(0)
-
-        for i in range(len(matrix_1)):
-            for j in range(len(matrix_1)):
-                for k in range(len(matrix_1)):
-                    end_matrix[i][j] += matrix_1[i][k] * matrix_2[k][j]
-
-        return end_matrix
-
     new_ = []
     for c in range(len(matrix)):
         new_.append([])
