a6ce71769b89dbf706518d7ed8a5b158c904853e
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-02-24 23:15:15 +0100

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -144,3 +144,13 @@ def solve_eq(matrix, result):
     return matrix, result
 
 print(solve_eq([[2,2],[2,1]],[6,5]))
+
+
+def cross_product(vector_1, vector_2):
+    new_vector = [1, 1, 1]
+
+    new_vector[0] *= vector_1[1]*vector_2[2] - vector_1[2]*vector_2[1]
+    new_vector[1] *= vector_1[2]*vector_2[0] - vector_1[0]*vector_2[2]
+    new_vector[2] *= vector_1[0]*vector_2[1] - vector_1[1]*vector_2[0]
+
+    return new_vector
