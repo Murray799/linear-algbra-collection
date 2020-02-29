36c18d34a88931942367a4968e45f3ba621e7d2e
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-02-29 23:49:50 +0100

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -1,3 +1,4 @@
+import math
 def dot_product(vector_1, vector_2):
     dot_product = 0
     for index in range(len(vector_1)):
@@ -153,10 +154,12 @@ def cross_product(vector_1, vector_2):
     return new_vector
 
 def absolute_value(vector):
-    import math
     val = 0
 
     for element in vector:
         val += element**2
 
     return math.sqrt(val)
+
+def angle(vector_1, vector_2):
+    return math.acos(dot_product(vector_2,vector_1)/absolute_value(vector_2)*absolute_value(vector_1))
