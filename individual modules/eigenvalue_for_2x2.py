560f56baaccd3070c3fb2c4df57f6d7a78757c86
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-03-21 00:53:26 +0100

    only for 2x2
    this is more of an experiment then anything else

---
+++ individual modules/eigenvalue_for_2x2.py
@@ -0,0 +1,12 @@
+def eigenvalue_2x2(matrix):
+    import math
+
+    result1 = (matrix[0][0] + matrix[1][1]) / 2 + math.sqrt(
+        ((matrix[0][0] + matrix[1][1]) / 2) ** 2 - matrix[0][0] * matrix[1][1])
+
+    result2 = (matrix[0][0] + matrix[1][1]) / 2 - math.sqrt(
+        ((matrix[0][0] + matrix[1][1]) / 2) ** 2 - matrix[0][0] * matrix[1][1])
+
+    return result1, result2
+
+print(eigenvalue_2x2([[1,2],[4,3]]))
