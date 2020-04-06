3cf8803e70fed7c51719b13f200d04cd4c600f0c
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-04-06 23:29:21 +0200

    Add files via upload

---
+++ individual modules/translation.py
@@ -0,0 +1,21 @@
+def translation(vectore, matrix):
+    def VM_multiplication(vector, matrix):
+        for element in range(len(vector)):
+            try:
+                for collum in range(len(matrix[element])):
+                    matrix[element][collum] = matrix[element][collum] * vector[element]
+            except IndexError:
+                ""
+
+        for new in range(len(vector)):
+            vector[new] = 0
+            try:
+                for col in range(len(matrix)):
+                    vector[new] += matrix[col][new]
+            except IndexError:
+                ""
+
+        return vector
+
+    return VM_multiplication(vectore, matrix)
+
