40da923d4b611e830a8f53ba2f5cb09ac2978488
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-04-14 00:05:15 +0200

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -1,4 +1,5 @@
 import math
+
 def eigenvalue_2x2(matrix):
     result1 = (matrix[0][0] + matrix[1][1]) / 2 + math.sqrt(
         ((matrix[0][0] + matrix[1][1]) / 2) ** 2 - matrix[0][0] * matrix[1][1])
