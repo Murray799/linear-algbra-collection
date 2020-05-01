73d7a326293da37f0a2cf6fbab5653290f8682be
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-05-01 23:50:49 +0200

    Update cross_product.py

--- individual modules/cross_product.py
+++ individual modules/cross_product.py
@@ -1,8 +1,9 @@
 def cross_product(vector_1, vector_2):
     new_vector = []
-
+    # defines empty list that will be contain the future end vectore 
+    
     new_vector.append(vector_1[1]*vector_2[2] - vector_1[2]*vector_2[1])
-    new_vector[1].append(vector_1[2]*vector_2[0] - vector_1[0]*vector_2[2])
-    new_vector[2].append(vector_1[0]*vector_2[1] - vector_1[1]*vector_2[0])
+    new_vector.append(vector_1[2]*vector_2[0] - vector_1[0]*vector_2[2])
+    new_vector.append(vector_1[0]*vector_2[1] - vector_1[1]*vector_2[0])
 
     return new_vector
