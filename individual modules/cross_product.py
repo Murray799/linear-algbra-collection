03819a8f8775b2bb58b6472934c10d2b5356f743
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-05-01 23:49:21 +0200

    Update cross_product.py

--- individual modules/cross_product.py
+++ individual modules/cross_product.py
@@ -1,7 +1,7 @@
 def cross_product(vector_1, vector_2):
     new_vector = []
 
-    new_vector[0] *= vector_1[1]*vector_2[2] - vector_1[2]*vector_2[1]
+    new_vector.append(vector_1[1]*vector_2[2] - vector_1[2]*vector_2[1])
     new_vector[1] *= vector_1[2]*vector_2[0] - vector_1[0]*vector_2[2]
     new_vector[2] *= vector_1[0]*vector_2[1] - vector_1[1]*vector_2[0]
 
