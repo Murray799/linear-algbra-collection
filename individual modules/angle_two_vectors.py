650586c3b2f7e6cb104861fac2e7a92c1a37b5d4
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-05-14 22:17:53 +0200

    Update angle_two_vectors.py

--- individual modules/angle_two_vectors.py
+++ individual modules/angle_two_vectors.py
@@ -4,6 +4,7 @@ def angle(vector_1, vector_2):
     def dot_product(vector_1, vector_2):
         dot_product = 0
         for index in range(len(vector_1)):
+            # interrates through both vectore at the same time
             try:
                 dot_product += vector_1[index] * vector_2[index]
             except IndexError:
