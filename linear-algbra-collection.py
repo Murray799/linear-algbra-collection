443946d2ecb08ad2ef6e295a12ead91377b60654
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-06-04 22:19:01 +0200

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -196,3 +196,16 @@ def eigen_vectore_2x2(matrix):
 def translation(vectore, matrix):
    new_result = VM_multiplication(vectore, matrix)
     return new_result
+
+def mid_vector(lis_vec):
+    # input must be list of vectors
+    new_vec = []
+    add = 0
+
+    for vec_row in range(len(lis_vec[0])):
+        for element in lis_vec:
+            add += element[vec_row]
+        new_vec.append(add / len(vec_row))
+        add = 0
+
+    return new_vec
