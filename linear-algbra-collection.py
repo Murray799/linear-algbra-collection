a610cb5f14aaeac100a71590c66c3a5aa9a4f5bc
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-06-04 21:59:23 +0200

    Update linear-algbra-collection.py

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -13,7 +13,7 @@ def dot_product(vector_1, vector_2):
     dot_product = 0
     for index in range(len(vector_1)):
         try:
-            dot_product += vector_1[index] * vector_2[index + 5]
+            dot_product += vector_1[index] * vector_2[index]
         except IndexError:
             ""
     
