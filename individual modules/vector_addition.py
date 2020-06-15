9f92e396a62fc09cce66dc4b9221b55712ddfa41
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-06-15 21:33:41 +0200

    Update vector_addition.py

--- individual modules/vector_addition.py
+++ individual modules/vector_addition.py
@@ -1,5 +1,6 @@
 def vector_addition(vector_1, vector_2):
     for element in range(len(vector_2)):
+        # goes through all elements in Vector
         try:
             vector_1[element] += vector_2[element]
         except IndexError:
