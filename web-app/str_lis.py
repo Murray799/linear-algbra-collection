0cab5d14a180d7f719ebb2c9d753c8b3fd1ff870
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-06-17 21:58:28 +0200

    Update str_lis.py

--- web-app/str_lis.py
+++ web-app/str_lis.py
@@ -1,22 +1,10 @@
-def string_lis(lis):
-    new_lis = []
-    for pos in range(len(lis)):
-        # goes throug each element of string
-        if pos == len(lis):
-            break
-        # if at the end ends vefore Index error
-
-        if lis[pos] == "[" or lis[pos] == ",":
-            # if start stop flag
-            if pos != 0:
-                new_lis.append(float(temp))
-                # add to temp if start flag as float
-            temp = ""
-            #wipe temp
-            pass
-
-        else:
-            temp += lis[pos]
-            # if not falg then part of number add it to tamp
-            
-    return new_lis
+def string_vector(lis):
+    lis = lis.strip("[")
+    lis = lis.strip("]")
+    # removes unnecessary elements from string
+    lis = lis.split(",")
+    # split in to individual numbers
+    for element in range(len(lis)):
+        lis[element] = float(lis[element])
+        # turns string in to numbers
+    return lis
