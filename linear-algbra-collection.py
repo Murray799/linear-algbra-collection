11189047740dd3ca80d7c125777fda8f3801437b
DonM19 <66700195+DonM19@users.noreply.github.com>
2020-07-14 17:15:05 +0200

    Comments
    copied comments from individual modules

--- linear-algbra-collection.py
+++ linear-algbra-collection.py
@@ -28,13 +28,16 @@ def dot_product(vector_1, vector_2):
 
 def VM_multiplication(vector, matrix):
     for element in range(len(vector)):
+    # goes through the matrix and then multiplies with correct vector element
         try:
             for column in range(len(matrix[element])):
                 matrix[element][column] = matrix[element][column]*vector[element]
         except IndexError:
             "incorrect dimensions please check input"
     for new in range(len(vector)):
+        # empties vector
         vector[new] = 0
+    # adds matrix columns together and then adds to vector
         try:
             for col in range(len(matrix)):
                 vector[new] += matrix[col][new]
