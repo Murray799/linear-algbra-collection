e053625a40d541edc8cce27ab673a709a2df8fde
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-07-20 18:03:46 +0200

    Update str_mat.py

--- web-app/str_mat.py
+++ web-app/str_mat.py
@@ -4,7 +4,6 @@ def string_matrix(mis):
     # removes unnecessary elements from string
     mis = mis.split(";")
     # splits it into individual vector parts
-
     for element in range(len(mis)):
         mis[element] = string_vector(mis[element])
         # turns string in to list
