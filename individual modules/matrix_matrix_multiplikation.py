03051661c54c952aa598d31fb2027f3047f43610
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-08-22 18:08:59 +0200

    Update matrix_matrix_multiplikation.py

--- individual modules/matrix_matrix_multiplikation.py
+++ individual modules/matrix_matrix_multiplikation.py
@@ -10,6 +10,7 @@ def MM_multiplication(matrix_1, matrix_2):
         for j in range(len(matrix_1)):
             for k in range(len(matrix_1)):
                 end_matrix[i][j] += matrix_1[i][k] * matrix_2[k][j]
+    # goes through each amtrix and adds there product to end matrix
 
     return end_matrix
 
