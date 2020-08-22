cfdcfe577b85811a3ac8cb182a7e96504c9b2432
Jean-Sim <59151535+Jean-Sim@users.noreply.github.com>
2020-08-22 18:07:07 +0200

    Update matrix_matrix_multiplikation.py

--- individual modules/matrix_matrix_multiplikation.py
+++ individual modules/matrix_matrix_multiplikation.py
@@ -4,6 +4,7 @@ def MM_multiplication(matrix_1, matrix_2):
         end_matrix.append([])
         for none in matrix_1:
             end_matrix[element].append(0)
+    # creates endmatrix
 
     for i in range(len(matrix_1)):
         for j in range(len(matrix_1)):
