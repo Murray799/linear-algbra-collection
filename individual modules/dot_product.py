e75ca542e4f3ed1d815e5b02a3e87794014835bf
Murray799 <66699976+Murray799@users.noreply.github.com>
2020-09-03 20:55:57 +0200

    Update dot_product.py

--- individual modules/dot_product.py
+++ individual modules/dot_product.py
@@ -3,7 +3,9 @@ def dot_product(vector_1, vector_2):
     for index in range(len(vector_1)):
         try:
             dot_product += vector_1[index] * vector_2[index]
+            # multiplies elements of the two vector and then adds it two dot_product
         except IndexError:
             "incorrect dimensions please check input"
+            # checks for IndexError  
 
     return dot_product
