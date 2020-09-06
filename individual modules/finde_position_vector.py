e5a50900c9843e231b260ffd3dc00bb4a3304d34
Murray799 <66699976+Murray799@users.noreply.github.com>
2020-09-06 19:33:16 +0200

    Update finde_position_vector.py

--- individual modules/finde_position_vector.py
+++ individual modules/finde_position_vector.py
@@ -1,5 +1,6 @@
 def finde_position_vector(point_1, point_2):
     for element in range(len(point_1)):
+        # goes through each element between those points and subtracts them 
         point_2[element] = point_2[element] - point_1[element]
 
     return point_2
